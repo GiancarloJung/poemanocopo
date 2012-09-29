5bd06610688dcb3b250c9da0affd8b6421a308f3
Giancarlo Jung <g8.jung@gmail.com>
2012-09-29 12:45:47 -0300

    layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poemanocopo::Application.config.secret_token = 'edcceb89c46a7ead70da94a81b1933a08850131488f969a24586ffeb5db085981d15fe9dd136c48ae4f591229320c4c734a85e66efe9b5fc43f38d969a5ab27a'
